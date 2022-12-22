cff678f86673890d65c10ed74d74a3ce1514ea96
xavi gomez <jumpering@gmail.com>
2022-12-22 22:45:49 +0100

    refactor, check requisites for define functionality design

--- src/App.java
+++ src/App.java
@@ -15,35 +15,42 @@ public class App {
     private final Mapper mapper = new Mapper();
 
     public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
-
         new App().run();
     }
 
     public void run() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
         Document document = new Document();
-        Header header = new Header();
-        Section section = new Section();
+        this.setDocumentAttributesAndHeaders(document);
+        this.setDetailToDocument(document);
+        System.out.println("screen print \n" + document.printConsole());
+        this.print(document);
+    }
 
-        NamedNodeMap documentNodeMap = this.xmlService.getAttributeNodeByTagName("Document");
-        this.mapper.mapAttributes(documentNodeMap, document);
+    private void setDocumentAttributesAndHeaders(Document document) throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        Header header = new Header();
+        this.mapper.mapAttributes(this.xmlService.getAttributeNodeByTagName("Document"), document);
         NodeList headerList = this.xmlService.getNodeListByTagName("Headers");
         this.mapper.mapValues(headerList, header);
         document.setHeader(header);
-        NodeList detailList = this.xmlService.getNodeListByTagName("Detail");
-        List<Node> nodeList = Mapper.getChildNodes(detailList);
+    }
+
+    private void setDetailToDocument(Document document) {
+        List<Node> detailNodeList = Mapper.getChildNodes(this.xmlService.getNodeListByTagName("Detail"));
         boolean sectionPresent = false;
-        for (int i = 0; i < nodeList.size(); i++) {
-            if (nodeList.get(i).getNodeName().equals("Section")) {
+        for (int i = 0; i < detailNodeList.size(); i++) {
+            if (detailNodeList.get(i).getNodeName().equals("Section")) {
                 sectionPresent = true;
             }
         }
 
         if (sectionPresent) {
-            NodeList sectionList = this.xmlService.getNodeListByTagName("Section");
-            this.mapper.mapSectionLineValues(sectionList, section);
+            Section section = new Section();
+            this.mapper.mapSectionLineValues(this.xmlService.getNodeListByTagName("Section"), section);
             document.setSection(section);
         }
-        System.out.println(document.printConsole());
-        System.out.println(document.getPrinter());
+    }
+
+    private void print(Document document) {
+        //todo send to printer
     }
 }
